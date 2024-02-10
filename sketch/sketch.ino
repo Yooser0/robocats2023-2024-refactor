b875ca2e7e0347716642f3515ed8cb88bb626d36
Yooser <crchampie@gmail.com>
2024-02-09 18:12:24 -0600

    Naming and logic issues in sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -29,43 +29,43 @@ void loop()
         switch (seedingState)
         {
         case SeedingRoundState::TURNING_TOWARDS_START_CORNER_FROM_START:
-            funcs.turningTowardsStartCornerFromStart();
+            seedingState = funcs.turningTowardsStartCornerFromStart();
             break;
         case SeedingRoundState::MOVING_TOWARDS_START_CORNER_FROM_START:
-            funcs.movingTowardsStartCornerFromStart();
+            seedingState = funcs.movingTowardsStartCornerFromStart();
             break;
-        case SeedingRoundState::TURNING_TOWARDS_BUTTON_CORNER_FROM_START:
-            funcs.turningTowardsButtonCornerFromStart();
+        case SeedingRoundState::TURNING_TOWARDS_BUTTON_CORNER_FROM_START_CORNER:
+            seedingState = funcs.turningTowardsButtonCornerFromStartCorner();
             break;
         case SeedingRoundState::MOVING_TOWARDS_BUTTON_CORNER_FROM_START_CORNER:
-            funcs.movingTowardsButtonCornerFromStartCorner();
+            seedingState = funcs.movingTowardsButtonCornerFromStartCorner();
             break;
         case SeedingRoundState::TURNING_TOWARDS_CENTER_FROM_BUTTON_CORNER:
-            funcs.turningTowardsCenterFromButtonCorner();
+            seedingState = funcs.turningTowardsCenterFromButtonCorner();
             break;
         case SeedingRoundState::MOVING_TOWARDS_CENTER_FROM_BUTTON_CORNER:
-            funcs.movingTowardsCenterFromButtonCorner();
+            seedingState = funcs.movingTowardsCenterFromButtonCorner();
             break;
         case SeedingRoundState::TURNING_TOWARDS_BUTTON_FROM_CENTER:
-            funcs.turningTowardsButtonFromCenter();
+            seedingState = funcs.turningTowardsButtonFromCenter();
             break;
         case SeedingRoundState::MOVING_TOWARDS_BUTTON_FROM_CENTER:
-            funcs.movingTowardsButtonFromCenter();
+            seedingState = funcs.movingTowardsButtonFromCenter();
             break;
         case SeedingRoundState::MOVING_TOWARDS_CENTER_FROM_BUTTON:
-            funcs.movingTowardsCenterFromButton();
+            seedingState = funcs.movingTowardsCenterFromButton();
             break;
         case SeedingRoundState::TURNING_TOWARDS_BUTTON_CORNER_FROM_CENTER:
-            funcs.turningTowardsButtonCornerFromCenter();
+            seedingState = funcs.turningTowardsButtonCornerFromCenter();
             break;
         case SeedingRoundState::MOVING_TOWARDS_BUTTON_CORNER_FROM_CENTER:
-            funcs.movingTowardsButtonCornerFromCenter();
+            seedingState = funcs.movingTowardsButtonCornerFromCenter();
             break;
         case SeedingRoundState::TURNING_TOWARDS_START_CORNER_FROM_BUTTON_CORNER:
-            funcs.turningTowardsStartCornerFromButtonCorner();
+            seedingState = funcs.turningTowardsStartCornerFromButtonCorner();
             break;
         case SeedingRoundState::MOVING_TOWARDS_START_CORNER_FROM_BUTTON_CORNER:
-            funcs.movingTowardsStartCornerFromButtonCorner();
+            seedingState = funcs.movingTowardsStartCornerFromButtonCorner();
             break;
         }
         break;
@@ -73,43 +73,43 @@ void loop()
         switch (elimState)
         {
         case EliminationRoundState::POWER_ON:
-            funcs.powerOn();
+            elimState = funcs.powerOn();
             break;
         case EliminationRoundState::MOVING_BACKWARDS_FROM_STATION:
-            funcs.movingBackwardsFromStation();
+            elimState = funcs.movingBackwardsFromStation();
             break;
         case EliminationRoundState::TURNING_AWAY_FROM_WALL:
-            funcs.turningAwayFromWall();
+            elimState = funcs.turningAwayFromWall();
             break;
         case EliminationRoundState::MOVING_AWAY_FROM_OBSTACLES:
-            funcs.movingAwayFromObstacles();
+            elimState = funcs.movingAwayFromObstacles();
             break;
         case EliminationRoundState::MOVING_TOWARDS_NEXT_STATION:
-            funcs.movingTowardsNextStation();
+            elimState = funcs.movingTowardsNextStation();
             break;
         case EliminationRoundState::TURNING_TOWARDS_PERPENDICULAR_BUTTON_WALL:
-            funcs.turningTowardsPerpendicularButtonWall();
+            elimState = funcs.turningTowardsPerpendicularButtonWall();
             break;
         case EliminationRoundState::MOVING_TOWARDS_PERPENDICULAR_BUTTON_WALL:
-            funcs.movingTowardsPerpendicularButtonWall();
+            elimState = funcs.movingTowardsPerpendicularButtonWall();
             break;
         case EliminationRoundState::MOVING_AWAY_FROM_PERPENDICULAR_BUTTON_WALL:
-            funcs.movingAwayFromPerpendicularButtonWall();
+            elimState = funcs.movingAwayFromPerpendicularButtonWall();
             break;
         case EliminationRoundState::TURNING_TOWARDS_CHARGER_FROM_CORNER:
-            funcs.turningTowardsChargerFromCorner();
+            elimState = funcs.turningTowardsChargerFromCorner();
             break;
         case EliminationRoundState::MOVING_TOWARDS_CHARGER_FROM_CORNER:
-            funcs.movingTowardsChargerFromCorner();
+            elimState = funcs.movingTowardsChargerFromCorner();
             break;
         case EliminationRoundState::CHARGING:
-            funcs.charging();
+            elimState = funcs.charging();
             break;
         }
         break;
     case Round::NONE:
         while (!Serial);
         Serial.println("Round not selected. Doing nothing.");
-        delay(LONG_MAX);
+        while (true);
     }
 }
